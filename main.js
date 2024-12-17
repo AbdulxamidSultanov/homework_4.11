d8b1be02cce998528d1ff3299b34155d3a611d91
itsforme <sultanovabdulxamid@gmail.com>
2024-12-17 18:35:20 +0500

    string va massivlar elelemtlari vazifasi tugatildi

--- main.js
+++ main.js
@@ -109,131 +109,174 @@ console.log(str1.charAt(res10));
 //Berilgan matnning birinchi belgisi uchun Unicode qiymatini aniqlang.
 const str2 = "Salom dunyo!";
 let res12 = str2.charCodeAt(0);
-console.log(res)
+console.log(res);
 // ============
 // 13-masala:
 //Matnning beshinchi belgisining Unicode qiymatini toping.
-const str3 = "Salom dunyo!"
-let res13 = str3.charCodeAt(4)
+const str3 = "Salom dunyo!";
+let res13 = str3.charCodeAt(4);
 // ============
 // 14-masala:
 //Berilgan matndan birinchi 5 ta belgini ajratib oling.
-const str4 = "Salom dunyo!"
-let res14 = str4.slice(0, 5)
-console.log(res14)
+const str4 = "Salom dunyo!";
+let res14 = str4.slice(0, 5);
+console.log(res14);
 // ============
 // 15-masala:
 //Matnning oxirgi 3 ta belgisini ajratib oling.
-const str5 = "Salom dunyo!"
-const arr15 = str5.split("")
-let lastThreeIndex = arr15.length - 3
-let res15 = str5.slice(lastThreeIndex, arr15.length)
-console.log(res15)
+const str5 = "Salom dunyo!";
+const arr15 = str5.split("");
+let lastThreeIndex = arr15.length - 3;
+let res15 = str5.slice(lastThreeIndex, arr15.length);
+console.log(res15);
 // ============
 // 16-masala:
 //Matnning 2-pozitsiyadan boshlab 6-pozitsiyagacha bo‘lgan qismini ajrating.
-
+const str6 = "Salom dunyo!";
+console.log(str6.substring(1, 6));
 // ============
 // 17-masala:
 //Matnning oxirgi uchta belgisini substring yordamida oling.
-
+const str7 = "Salom dunyo!";
+const arr17 = str7.split("");
+let lastIndex17 = arr17.length - 3;
+console.log(str7.substring(arr17.length, lastIndex17));
 // ============
 // 18-masala:
 //Matnning 4-pozitsiyadan boshlab 5 ta belgisini oling.
-
+const str8 = "Salom dunyo!";
+console.log(str8.substr(4, 5));
 // ============
 // 19-masala:
 //Berilgan matnning boshidan 7 ta belgisini substr yordamida ajrating.
-
+const str9 = "Matn berilgan shuni boshidan yetitasini ajratish";
+console.log(str9.substr(0, 7));
 // ============
 // 20-masala:
 //Berilgan matnni faqat katta harflar bilan yozing.
-
+const str10 = "Matn berilgan shuni boshidan yetitasini ajratish";
+let res20 = str10.toUpperCase();
 // ============
 // 21-masala:
 //Matnning birinchi yarmidan kichik harflar, ikkinchi yarmidan katta harflar yarating.
-
+const str11 = "salom dunyo!";
+const arr21 = str11.split("");
+let indexArr = Math.trunc(arr21.length / 2);
+let res21 =
+  str11.slice(0, indexArr) + " " + str11.slice(indexArr).toUpperCase();
 // ============
 // 22-masala:
 //Berilgan ikkita matnni birlashtiring.
-
+const str12 = "Salom";
+const newStr12 = " dunyo!";
+let res22 = str12.concat(newStr12);
 // ============
 // 23-masala:
 //Matnga qo‘shimcha qilib "!" belgisi qo‘shing va yangi matn yarating.
-
+const str13 = "Salom dunyo";
+let res23 = str13.concat("!");
 // ============
 // 24-masala:
 //Matnning boshidagi va oxiridagi bo‘sh joylarni olib tashlang.
-
+const str14 = "   Salom Dunyo!   ";
+let res24 = str14.trim();
 // ============
 // 25-masala:
 //Matnning faqat boshidagi bo‘sh joylarni olib tashlang.
-
+const str15 = "   Salom Dunyo!   ";
+let res25 = str14.trimStart();
 // ============
 // 26-masala:
 //Berilgan matnni boshidan 10 belgiga to‘ldiring, “*” belgisi bilan.
-
+const str16 = "Salom dunyo!";
+const arr26 = str16.split("");
+let arrLength = arr26.length + 10;
+let res26 = str16.padStart(arrLength, "*");
 // ============
 // 27-masala:
 //Matnni oxiridan 15 belgiga “#” bilan to‘ldiring.
-
+const str17 = "Salom dunyo!";
+const arr27 = str17.split("");
+let lengthArr = arr27.length + 15;
+let res27 = str17.padEnd(lengthArr, "*");
 // ============
 // 28-masala:
 //Matnni 3 marta takrorlab yangi matn yarating.
-
+const str18 = "Salom dunyo!";
+let repeatStr = str18.repeat(3);
 // ============
 // 29-masala:
 //Belgini 5 marta takrorlab, string yaratib oling.
-
+const str19 = "*";
+let reapetZnak = str19.repeat(5);
 // ============
 // 30-masala:
 //Matnning ichidagi birinchi "a" harfini "o" harfiga almashtiring.
-
+const str20 = "Salom Dunyo!";
+let changeLetters = str20.replace("a", "o");
 // ============
 // 31-masala:
 //Matnning barcha “ ” (bo‘sh joy) belgilarini “_” belgilariga almashtiring.
-
+const str21 = "Matining barcha bosh joyalrini _ chiziqga alishtirish";
+let changeWhiteSpace = str21.replaceAll(" ", "_");
 // ============
 // 32-masala:
 //Matnni har bir so‘zdan ajratib massivga aylantiring.
-
+const str22 = "Matnni har bir so`zdan ajratib massivga aylantiring.";
+let res32 = str22.split(" ");
+console.log(res32);
 // ============
 // 33-masala:
 //Matnni har bir belgidan ajratib massiv yarating.
-
+const str23 = "Matnni har bir belgidan ajratib massiv yarating.";
+let res33 = str23.split("");
 // ============
 // 34-masala:
 //Matndagi "a" harfi birinchi marta qayerda uchrayotganini aniqlang.
-
+const str24 = "Salom dunyo!";
+let res34 = str24.indexOf(a);
+console.log(res34);
 // ============
 // 35-masala:
 //Matndagi "e" harfi oxirgi marta qayerda uchrayotganini toping.
-
+const str25 = "Matndagi e harfi oxirgi marta qayerda uchrayotganini toping.";
+let res35 = str25.lastIndexOf("e");
 // ============
 // 36-masala:
 //Matn ichida "hello" so‘zi bor-yo‘qligini tekshiring.
-
+const str26 = "Hello, world!";
+let res36 = str26.includes("hello"); // false chiqadi chunki hello tepada kotta harifda kelgan
 // ============
 // 37-masala:
 //Matn ichida "!" belgisi mavjudligini aniqlang.
-
+const str27 = "Hello, world!";
+let res37 = str27.includes("!");
 // ============
 // 38-masala:
 //Matn ichida birinchi "good" so‘zi qayerda joylashganini aniqlang.
-
+const str28 = "Hello, I`m good. And you?";
+let res38 = str28.search("good");
 // ============
 // 39-masala:
 //Matnda raqam uchraydigan joyni aniqlang.
-
+const str29 = "Hello, My name is Rovshan, I`m 99 years old";
+const arr39 = str29.split(" ");
+for (let i = 0; i < arr.length; i++) {
+  if (!isNaN(arr[i])) {
+    console.log(arr.indexOf(arr[i]));
+  }
+}
 // ============
 // 40-masala:
 //Matn "Hello" so‘zi bilan boshlanadimi yoki yo‘qmi, tekshiring.
-
+const str30 = "Hello, world!";
+let res40 = str30.startsWith("Hello");
 // ============
 // 41-masala:
 //Matn oxirida "bye" so‘zi borligini aniqlang.
 // ============
-
+const str31 = "Hello, My name is Rovshan, I`m 99 years old, It`s all, bye";
+let res41 = str31.endsWith("bye");
 // 42-masala:
 //users massivida age qiymati 25 dan katta bo‘lgan foydalanuvchilarni toping.
 let users = [
